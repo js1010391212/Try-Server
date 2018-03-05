d1f5154cea71ccf560692bfd6b8247e5c6ddf0b6
js1010391212 <1010391212@qq.com>
2018-03-05 13:27:54 +0800

    终于写完了

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
-#include "server.h"
+#include "main.h"
 
-//åˆå§‹åŒ–æˆå‘˜å˜é‡
+//³õÊ¼»¯³ÉÔ±±äÁ¿
 void InitMember(void)
 {
-	//åˆå§‹åŒ–è¯»å†™ç¼“å­˜
+	//³õÊ¼»¯¶ÁĞ´»º´æ
 	ZeroMemory(bufRecv, sizeof(bufRecv));
 	ZeroMemory(bufSend, sizeof(bufSend));
 
-	//åˆå§‹åŒ–
+	//³õÊ¼»¯
 	sServer = INVALID_SOCKET;
 	sClient = INVALID_SOCKET;
 	bConning = false;
 	retVal = 0;
 }
 
-//é”™è¯¯å¤„ç†
+//µ¯³öÏûÏ¢´¦Àí
+void ShowSocketMsg(char *str)
+{
+	MessageBox(NULL, TEXT("%s", str), TEXT("Error!"), MB_OKCANCEL);
+}
+
+//´íÎó´¦Àí
 int HandleSocketError(char *str)
 {
 	ShowSocketMsg(str);
@@ -22,58 +28,191 @@ int HandleSocketError(char *str)
 	return SERVER_API_ERROR;
 }
 
-//å¼¹å‡ºæ¶ˆæ¯å¤„ç†
-int ShowSocketMsg(char *str)
+BOOL WsaStartup(void)
 {
-	MessageBox(NULL, TEXT("%s", str), TEXT("Error!"), MB_OKCANCEL);
-}
-
-bool WsaStartup(void)
-{
-	//åˆå§‹åŒ–Windows sockets DLL
+	//³õÊ¼»¯Windows sockets DLL
 	WORD requse = MAKEWORD(2, 2);
 	retVal = WSAStartup(requse, &wsaData);
-	if (retVal!=0)
+	if (retVal != 0)
 	{
 		printf("can not find a usable windows sockets dll!");
 		return SERVER_DLL_ERROR;
 	}
-	//ç¡®ä¿Windows DLLæ”¯æŒ1.1
+	//È·±£Windows DLLÖ§³Ö1.1
 	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
 	{
 		ShowSocketMsg("Can not find a usable Windows Sockets dll!");
 		WSACleanup();
 		return SERVER_DLL_ERROR;
 	}
-	return true;
+	return TRUE;
 }
 
-//åˆ›å»ºå¥—æ¥å­—
-bool Scokets(void)
+//´´½¨Ì×½Ó×Ö
+BOOL Scokets(void)
 {
 	sServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (INVALID_SOCKET==sServer)
+	if (INVALID_SOCKET == sServer)
 	{
-		 return HandleSocketError("Failed socket()!");
+		return HandleSocketError("Failed socket()!");
 	}
-	return true;
+	return TRUE;
 }
 
-//ç»‘å®šå¥—æ¥å­—
-bool Bind_socket(void)
+//°ó¶¨Ì×½Ó×Ö
+BOOL Bind_socket(void)
 {
-	//æœåŠ¡å™¨å¥—æ¥å­—åœ°å€
+	//·şÎñÆ÷Ì×½Ó×ÖµØÖ·
 	SOCKADDR_IN addrServ;
 	addrServ.sin_family = AF_INET;
-	addrServ.sin_port =htons(SERVER_PORT);
+	addrServ.sin_port = htons(SERVER_PORT);
 	addrServ.sin_addr.S_un.S_addr = INADDR_ANY;
 
-	//ç»‘å®šå¥—æ¥å­—
+	//°ó¶¨Ì×½Ó×Ö
 	retVal = bind(sServer, (LPSOCKADDR)&addrServ, sizeof(addrServ));
-	if (SOCKET_ERROR==retVal)
+	if (SOCKET_ERROR == retVal)
 	{
 		closesocket(sServer);
 		return HandleSocketError("Failed bind()!");
 	}
-	return true;
+	return TRUE;
+}
+
+//¼àÌı
+BOOL Listen_S(void)
+{
+	retVal = listen(sServer, 5);
+	if (retVal==SOCKET_ERROR)
+	{
+		closesocket(sServer);
+		return HandleSocketError("Filed listen()");
+	}
+	printf("Server succeed!\n");
+	printf("Waiting for new cilent()!\n");
+	return TRUE;
+}
+
+//½ÓÊÜÇëÇó
+void Accept(void)
+{
+	sockaddr_in Cclient;
+	int CclientLen = sizeof(Cclient);
+	sClient = accept(sServer, (sockaddr FAR*)&Cclient, &CclientLen);
+	if (INVALID_SOCKET)
+	{
+		closesocket(sServer);
+		HandleSocketError("flied accept()!");
+	}
+	else
+	{
+		bConning = true;
+	}
+}
+
+//½ÓÊÜÊı¾İ
+BOOL RecvLine(SOCKET s,char* buf)
+{
+	BOOL retVal = TRUE;				//·µ»ØÖµ
+	BOOL bLineEnd = FALSE;			//ĞĞ½áÊø
+	int nReadLen = 0;				//¶ÁÈë×Ö½ÚÊı
+	int nDataLen = 0;				//Êı¾İ³¤¶È
+
+	while (!bLineEnd&&bConning)
+	{
+		nReadLen = recv(s, buf + nDataLen, 1, 0);
+		//´íÎó´¦Àí
+		if (SOCKET_ERROR==nReadLen)
+		{
+			int nErrCode = WSAGetLastError();	//´íÎó´úÂë
+			if (WSAENOTCONN==nErrCode)
+			{
+				ShowSocketMsg("The socket is not connected!");
+			}
+			else if (WSAESHUTDOWN==nErrCode)
+			{
+				ShowSocketMsg("The socket has been shup down!");
+			}
+			else if (WSAETIMEDOUT==nErrCode)
+			{
+				ShowSocketMsg("The socket has been dropped!");
+			}
+			else if (WSAECONNRESET==nErrCode)
+			{
+				ShowSocketMsg("ERROR!!!");
+			}
+			else{}
+
+			retVal = FALSE;		//¶ÁÊı¾İÊ§°Ü
+			break;
+		}
+		//¿Í»§¶Ë¹Ø±Õ
+		if (0==nReadLen)
+		{
+			retVal = FALSE;
+			break;
+		}
+		//¶ÁÈëÊı¾İ
+		if ('\n'==*(buf+nDataLen))
+		{
+			bLineEnd = TRUE;	//½ÓÊÜÊı¾İ½áÊø
+		}
+		else
+		{
+			nDataLen += nReadLen;
+		}
+	}
+	return retVal;
+}
+
+//·¢ËÍÊı¾İ
+BOOL SendLine(SOCKET s, char* str)
+{
+	int retVal;		//·µ»ØÖµ
+	retVal = send(s, str, strlen(str), 0);	//Ò»´Î·¢ËÍ
+
+	//´íÎó´¦Àí
+	if (SOCKET_ERROR==retVal)
+	{
+		int nErrCode = WSAGetLastError();	//´íÎó´úÂë
+		if (WSAENOTCONN == nErrCode)
+		{
+			ShowSocketMsg("The socket is not connected!");
+		}
+		else if (WSAESHUTDOWN == nErrCode)
+		{
+			ShowSocketMsg("The socket has been shup down!");
+		}
+		else if (WSAETIMEDOUT == nErrCode)
+		{
+			ShowSocketMsg("The socket has been dropped!");
+		}else{}
+
+		return FALSE;	
+	}
+	return TRUE;
+}
+
+//ÍË³ö
+int ExitClient(int nExit)
+{
+	closesocket(sServer);
+	closesocket(sClient);
+	WSACleanup();
+	return nExit;
+}
+//Ö÷º¯ÊıÈë¿Ú
+int main(int argc, char* argv[])
+{
+	WsaStartup();
+	Scokets();
+	Bind_socket();
+	Listen_S();
+	Accept();
+	RecvLine(sClient, bufRecv);
+	SendLine(sClient, bufSend);
+	CopyMemory(bufSend, "hello client", sizeof(bufSend));
+	if (!SendLine(sClient,bufSend))
+	{
+		return ExitClient(SERVER_API_ERROR);
+	}
 }
